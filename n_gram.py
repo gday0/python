1c3cf1b02bd1b938aecfa4607343c5b40edba017
gday0 <gday8087@gmail.com>
2018-04-18 15:37:28 +0900

    use pickle module

--- n_gram.py
+++ n_gram.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle   # 파이썬 객체를 파일에 저장할 때 pickle 모듈의 dump 사용
 
 with open('words2.txt', 'r') as file:
     text = file.read()
@@ -7,9 +7,8 @@ with open('words2.txt', 'r') as file:
 
     for word in words:
         #print(word)
-        
+
         for i in range(len(word) // 2):
             if word[i] != word[-1 - i]:
                 break
-        print(word)    
-                       
+        print(word)
