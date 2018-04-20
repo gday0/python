07f0d5aa97e2beee3403198946e47797c45b9414
gday0 <gday8087@gmail.com>
2018-04-20 14:01:21 +0900

    34.5 파일 이름 한꺼번에 바꾸기
    finished

--- lambda_practice.py
+++ lambda_practice.py
@@ -1,5 +1,7 @@
 # 심사문제: 파일명 형식 적용하기. 파일명 숫자 3개.확장자(xxx.xxx)
 files = input().split()
 
-print(list(map(lambda files: '{0:03d}.{1}',
-    format(int(files.split('.')[0]), str(files.split('.')[1])))))
+# lambda 인수: 결과
+# map(함수, 변수)
+# 결과값 출력을 위하여 list 객체로 변환
+print(list(map(lambda x: '{0:03d}.{1}'.format(int(x.split('.')[0]), str(x.split('.')[1])), files)))
