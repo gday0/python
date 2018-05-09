360d48ca386edb7619d8e4600da0c412ae7cf7da
gday0 <gday8087@gmail.com>
2018-05-09 11:30:48 +0900

    @staticmethod @classmethod

--- class_method.py
+++ class_method.py
@@ -5,10 +5,10 @@ class Person:
         Person.bag.append(stuff)
 
 a = Person()
-a.put_bag('지갑')
+a.put_bag('wallet')
 
 b = Person()
-b.put_bag('이어폰')
+b.put_bag('earphone')
 
 print(a.bag, b.bag)
 print(Person.bag)
@@ -17,16 +17,32 @@ print(Person.bag)
 # 인스턴스 속성으로 만들면 된다
 class Person1:
     def __init__(self):
+        '''인스턴스 만들어질 때 생성'''
         self.bag = []
+    # __속성 = 값 # 비공개 클래스 속성
 
     def put_bag(self, stuff):
+        '''인사 메서드'''
         self.bag.append(stuff)
 
 a = Person1()
-a.put_bag('지갑')
+a.put_bag('wallet')
 
 b = Person1()
-b.put_bag('이어폰')
+b.put_bag('earphone')
 
 print(a.bag)
 print(b.bag)
+
+# 정적 메서드
+class Calc:
+    @staticmethod   # 인스턴스 내용 상관없는 결과값 필요할 때
+    def add(a, b):
+        print(a+b)
+
+    @staticmethod
+    def mul(a, b):
+        print(a*b)
+
+Calc.add(3, 35)
+Calc.mul(4, 12)
